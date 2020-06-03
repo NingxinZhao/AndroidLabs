cad84a2b6d38b7c4c258c8266806bf65649ddda6
NingxinZhao <zhao0191@algonquinlive.com>
2020-06-03 15:00:37 -0400

    lab2_2

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
 
         Button btn = findViewById(R.id.checkBox2);
         btn.setOnClickListener(new View.OnClickListener() {
